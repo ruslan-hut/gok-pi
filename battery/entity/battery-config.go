7fffd120c4fb17df308bbbfbf334da2be7eaaf15
ruslanhut <dev@programmer.com.ua>
2025-11-15 10:57:59 +0100

    Update configuration structs to support JSON serialization
    - Added JSON tags to BatteryConfig and Schedule structs for improved compatibility with JSON data handling.
    - Ensured existing YAML tags remain intact for backward compatibility with YAML configurations.

--- battery/entity/battery-config.go
+++ battery/entity/battery-config.go
@@ -1,9 +1,9 @@
 package entity
 
 type BatteryConfig struct {
-	Name          string `yaml:"name" env-default:"battery1"`
-	Url           string `yaml:"url" env-default:"https://example.battery/api"`
-	Token         string `yaml:"token" env-default:"auth-token"`
-	Enabled       bool   `yaml:"enabled" env-default:"true"`
-	CapacityLimit int    `yaml:"capacity_limit" env-default:"20000"`
+	Name          string `yaml:"name" json:"name" env-default:"battery1"`
+	Url           string `yaml:"url" json:"url" env-default:"https://example.battery/api"`
+	Token         string `yaml:"token" json:"token" env-default:"auth-token"`
+	Enabled       bool   `yaml:"enabled" json:"enabled" env-default:"true"`
+	CapacityLimit int    `yaml:"capacity_limit" json:"capacity_limit" env-default:"20000"`
 }
--- battery/entity/schedule-config.go
+++ battery/entity/schedule-config.go
@@ -1,11 +1,11 @@
 package entity
 
 type Schedule struct {
-	Name        string `yaml:"name" env-default:""`
-	StartTime   string `yaml:"start_time" env-default:"18:00"`
-	StopTime    string `yaml:"stop_time" env-default:"22:00"`
-	BatteryName string `yaml:"battery_name" env-required:"battery1"`
-	Enabled     bool   `yaml:"enabled" env-default:"false"`
-	PowerLimit  int    `yaml:"power_limit" env-default:"1000"`
-	SocLimit    int    `yaml:"soc_limit" env-default:"50"`
+	Name        string `yaml:"name" json:"name" env-default:""`
+	StartTime   string `yaml:"start_time" json:"start_time" env-default:"18:00"`
+	StopTime    string `yaml:"stop_time" json:"stop_time" env-default:"22:00"`
+	BatteryName string `yaml:"battery_name" json:"battery_name" env-required:"battery1"`
+	Enabled     bool   `yaml:"enabled" json:"enabled" env-default:"false"`
+	PowerLimit  int    `yaml:"power_limit" json:"power_limit" env-default:"1000"`
+	SocLimit    int    `yaml:"soc_limit" json:"soc_limit" env-default:"50"`
 }
